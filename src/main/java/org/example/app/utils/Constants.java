e843f99cd33c68e0085d798c994b85e4df916dd6
AdamMudrak <mudrets2000@gmail.com>
2023-04-19 21:11:34 +0300

    Add regex and message for email

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -24,6 +24,9 @@ public final class Constants {
     public final static String WRONG_ID_MSG = "Wrong id value.";
     public final static String ID_NO_EXISTS_MSG = "There is no such ID.";
 
+    public final static String EMAIL_RGX = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    public final static String WRONG_EMAIL_MSG = "Wrong email.";
+
     // Log messages
     public final static String LOG_DB_ABSENT_MSG = "Database is absent!";
     public final static String LOG_DATA_INPTS_WRONG_MSG = "Data inputs incorrect.";
